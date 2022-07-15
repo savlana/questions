51d82c6f9dc5e8a54e77062c4a5031784dd25d0f
savlana <108239589+savlana@users.noreply.github.com>
2022-07-15 21:30:53 +0300

    Add files via upload

---
+++ js.info.js
@@ -0,0 +1,5 @@
+alert('Брэндан Эйх разработал язык программирования JavaScript');
+alert('JavaScript был разработан за 10 дней');
+alert('JavaScript появился в 1995 году');
+alert('JavaScript - это не Java');
+alert('Cпецификация языка JavaScript называется ECMAScript');
